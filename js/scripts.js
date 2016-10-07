fdbc995589fe2196c3ee7c20973417f15884a4e8
Xiaoxi Zheng <shoush0713@gmail.com>
2016-10-07 15:35:46 -0400

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+});
+
+$(document).ready(function(){
+  $("#counter").countdown({
+  until: new Date(2017, 3 - 1, 15),
+  format: 'dHMS'
+  });
+
+  $("#counter_wrapper").fitText(1.5, {
+  minFontSize: '20px',
+  maxFontSize: '40px'
+  });
+});
