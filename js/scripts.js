ba61b063648c8dcbf5e5ec84a24cf7f1baf22ebc
Xiaoxi Zheng <shoush0713@gmail.com>
2016-11-15 20:54:13 -0500

    debugging2

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
   $("#counter").countdown({
-  until: new Date(2017, 3 - 2, 0),
+  until: new Date(2017, 2- 3, 0),
   format: 'dHMS'
   });
 
